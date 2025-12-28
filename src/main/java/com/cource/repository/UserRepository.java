c8024d4d6a919be90cddaeee0b01a719c677ff56
LOU Chumdararith <louchumdararith02@gmail.com>
2025-12-28 23:26:54 +0700

    fix(core): replace findByUsername with findByEmail to match User entity

--- src/main/java/com/cource/repository/UserRepository.java
+++ src/main/java/com/cource/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cource.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 
     List<User> findByRole_RoleName(String roleName);
 
