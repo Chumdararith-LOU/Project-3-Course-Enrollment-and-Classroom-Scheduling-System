ec1d10d534d835648b003ca87e1088b67ed719b7
Axgust22 <cheangseyha2208@gmail.com>
2025-12-28 15:33:31 +0700

    Adding Controller for Feature: Admin

--- src/main/java/com/cource/controller/CourseController.java
+++ src/main/java/com/cource/controller/CourseController.java
@@ -1,5 +1,12 @@
 package com.cource.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import com.cource.service.CourseService;
+
+@RestController
+@RequiredArgsConstructor
 public class CourseController {
-    
+    private final CourseService courseService;
+
 }
