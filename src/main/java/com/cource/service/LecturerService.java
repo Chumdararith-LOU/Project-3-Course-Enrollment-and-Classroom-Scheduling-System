eb62a145bdda9e10b8638b9516bb438dfef43de4
Axgust22 <cheangseyha2208@gmail.com>
2026-01-04 17:46:12 +0700

    refactor(service): format LecturerService interface by adjusting spacing and organizing method declarations

--- src/main/java/com/cource/service/LecturerService.java
+++ src/main/java/com/cource/service/LecturerService.java
@@ -7,19 +7,21 @@ import com.cource.dto.course.CourseResponseDTO;
 import com.cource.dto.lecturer.LecturerDashboardDTO;
 import com.cource.entity.Attendance;
 import com.cource.entity.ClassSchedule;
-import com.cource.entity.Course;
 import com.cource.entity.User;
 
 public interface LecturerService {
     LecturerDashboardDTO getDashboardStats(Long lecturerId);
 
     List<CourseResponseDTO> getCoursesByLecturerId(long lecturerId);
+
     List<ClassSchedule> getClassSchedulesByLecturerId(long offeringId, long lecturerId);
+
     List<User> getEnrolledStudents(long offeringId, long lecturerId);
 
     void recordAttendance(AttendanceRequestDTO attendanceRequestDTO, long studentId, String status, long lecturerId);
 
     List<Attendance> getAttendanceRecords(long scheduleId, long lecturerId);
+
     String regenerateOfferingCode(Long offeringId);
 
 }
