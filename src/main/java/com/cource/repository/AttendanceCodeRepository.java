5e8804a98117c28a1da66345ca74c741105508ca
LOU Chumdararith <louchumdararith02@gmail.com>
2026-01-15 14:08:15 +0700

    fix(repo): update AttendanceCode Repository to align with new refactored database

--- src/main/java/com/cource/repository/AttendanceCodeRepository.java
+++ src/main/java/com/cource/repository/AttendanceCodeRepository.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cource.entity.AttendanceCode;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AttendanceCodeRepository extends JpaRepository<AttendanceCode, Long> {
-    Optional<AttendanceCode> findByScheduleId(Long scheduleId);
+    Optional<AttendanceCode> findBySchedule_Id(Long scheduleId);
 
+    @Modifying
+    @Query("DELETE FROM AttendanceCode ac WHERE ac.schedule.id = :scheduleId")
     void deleteByScheduleId(Long scheduleId);
 }
